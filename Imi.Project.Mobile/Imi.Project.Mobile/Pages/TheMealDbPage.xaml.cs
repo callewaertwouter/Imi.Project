87710c999c9396581c5b29eed6cef9a5f254ff79
woutercallewaert <wouter.callewaert@student.howest.be>
2023-09-20 17:13:28 +0200

    Create external API page

---
+++ Imi.Project.Mobile/Imi.Project.Mobile/Pages/TheMealDbPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Imi.Project.Mobile.ViewModels;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.Pages
+{
+    public partial class TheMealDbPage : ContentPage
+    {
+        private TheMealDbViewModel viewModel;
+
+        public TheMealDbPage()
+        {
+            InitializeComponent();
+            viewModel = new TheMealDbViewModel();
+            BindingContext = viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await viewModel.LoadRecipesAsync();
+        }
+    }
+}
