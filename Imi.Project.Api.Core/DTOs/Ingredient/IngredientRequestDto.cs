b6a67ad09d3b93beefaee84b24f886be089efc9b
woutercallewaert <wouter.callewaert@student.howest.be>
2023-09-27 17:52:21 +0200

    Add ingredient DTOs

---
+++ Imi.Project.Api.Core/DTOs/Ingredient/IngredientRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Imi.Project.Api.Core.DTOs.Ingredient;
+
+public class IngredientRequestDto
+{
+    public string Name { get; set; }
+}
---
+++ Imi.Project.Api.Core/DTOs/Ingredient/IngredientResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using Imi.Project.Api.Core.DTOs.Recipe;
+using System;
+using System.Collections.Generic;
+
+namespace Imi.Project.Api.Core.DTOs.Ingredient;
+
+public class IngredientResponseDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public double Quantity { get; set; }
+    public string MeasureUnit { get; set; }
+    public ICollection<RecipeResponseDto> UsedInRecipes { get; set; }
+}
