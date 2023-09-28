b4ba094eb8c2896ae1b957c114f94d50fe70838c
woutercallewaert <wouter.callewaert@student.howest.be>
2023-09-28 23:38:47 +0200

    Add properties

--- Imi.Project.Api.Core/DTOs/Recipe/RecipeRequestDto.cs
+++ Imi.Project.Api.Core/DTOs/Recipe/RecipeRequestDto.cs
@@ -1,6 +1,13 @@
-﻿namespace Imi.Project.Api.Core.DTOs.Recipe;
+﻿using Imi.Project.Api.Core.DTOs.Ingredient;
+using System;
+using System.Collections.Generic;
+
+namespace Imi.Project.Api.Core.DTOs.Recipe;
 
 public class RecipeRequestDto
 {
+    public Guid Id { get; set; }
     public string Title { get; set; }
+    public string Description { get; set; }
+    public ICollection<IngredientRequestDto> ListOfIngredients { get; set; }
 }
--- Imi.Project.Api.Core/DTOs/User/UserRequestDto.cs
+++ Imi.Project.Api.Core/DTOs/User/UserRequestDto.cs
@@ -1,6 +1,9 @@
-﻿namespace Imi.Project.Api.Core.DTOs.User;
+﻿using System;
+
+namespace Imi.Project.Api.Core.DTOs.User;
 
 public class UserRequestDto
 {
+    public Guid Id { get; set; }
     public string Email { get; set; }
 }
