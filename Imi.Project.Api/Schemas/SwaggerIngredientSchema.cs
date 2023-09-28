ac5bb5521952a42061a7eecbc46a95dd9b0e6a3a
woutercallewaert <wouter.callewaert@student.howest.be>
2023-09-28 17:27:26 +0200

    Add swagger schemas

---
+++ Imi.Project.Api/Schemas/SwaggerIngredientSchema.cs
@@ -0,0 +1,21 @@
+﻿using Imi.Project.Api.Core.DTOs.Ingredient;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Imi.Project.Api.Schemas;
+
+public class SwaggerIngredientSchema : ISchemaFilter
+{
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        Random random = new Random();
+        var randomNumber = random.Next(0, 1000);
+
+        if (context.Type == typeof(IngredientRequestDto))
+        {
+            schema.Type = "object";
+            schema.Properties["name"].Example = new OpenApiString("TestIngredient" + randomNumber); 
+        }
+    }
+}
---
+++ Imi.Project.Api/Schemas/SwaggerUserRegisterSchema.cs
@@ -0,0 +1,25 @@
+﻿using Imi.Project.Api.Core.DTOs.Ingredient;
+using Imi.Project.Api.Core.DTOs.User;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Imi.Project.Api.Schemas;
+
+public class SwaggerUserRegisterSchema : ISchemaFilter
+{
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        Random random = new Random();
+        var randomNumber = random.Next(0, 1000);
+
+        if (context.Type == typeof(RegisterUserRequestDto))
+        {
+            schema.Type = "object";
+            schema.Properties["email"].Example = new OpenApiString($"test{randomNumber}@imi.be");
+            schema.Properties["userName"].Example = new OpenApiString("");
+            schema.Properties["password"].Example = new OpenApiString($"Test123?");
+            schema.Properties["confirmPassword"].Example = new OpenApiString($"Test123?");
+        }
+    }
+}
