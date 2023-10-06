ebd73d9a003ee5604b5a441deb5abe43c4770982
woutercallewaert <wouter.callewaert@student.howest.be>
2023-10-06 07:18:33 +0200

    Inject services

--- Imi.Project.Blazor/Program.cs
+++ Imi.Project.Blazor/Program.cs
@@ -1,10 +1,27 @@
-using Imi.Project.Blazor.Data;
+using Imi.Project.Api.Core.Entities;
+using Imi.Project.Blazor.Models.Mocking;
+using Imi.Project.Blazor.Models;
+using Imi.Project.Blazor.Services.Api;
+using Imi.Project.Blazor.Services.Crud;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddTransient<ICRUDService<MockRecipe>, BlazorRecipeService>();
+builder.Services.AddTransient<ICRUDService<MockUser>, BlazorUserService>();
+builder.Services.AddTransient<ICRUDService<MockIngredient>, BlazorIngredientService>();
+
+builder.Services.AddTransient<ICRUDService<User>, UserApiService>();
+builder.Services.AddTransient<ICRUDService<Recipe>, RecipeApiService>();
+builder.Services.AddTransient<ICRUDService<Ingredient>, IngredientApiService>();
+
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped<HttpClient>();
+builder.Services.AddSingleton<HttpClient>();
 
 var app = builder.Build();
 
