feacbb115e2f2fb0cb980360d0d2d40287498341
woutercallewaert <wouter.callewaert@student.howest.be>
2023-10-08 23:49:25 +0200

    Cleanup

--- Imi.Project.Vue/Models/ErrorViewModel.cs
+++ Imi.Project.Vue/Models/ErrorViewModel.cs
@@ -4,7 +4,7 @@ namespace Imi.Project.Vue.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
