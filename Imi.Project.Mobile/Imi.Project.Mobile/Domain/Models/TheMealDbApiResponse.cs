348d7f86671e1c619a8311c11e5c9061565181f1
woutercallewaert <wouter.callewaert@student.howest.be>
2023-10-08 23:49:17 +0200

    Add external api models

---
+++ Imi.Project.Mobile/Imi.Project.Mobile/Domain/Models/TheMealDbApiResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Imi.Project.Mobile.Domain.Models
+{
+	public class ExternalApiResponse
+	{
+		public List<Meal> Meals { get; set; }
+
+	}
+
+	public class Meal
+	{
+		public string IdMeal { get; set; }
+		public string StrMeal { get; set; }
+        public string StrCategory { get; set; }
+        public string StrInstructions { get; set; }
+	}
+}
---
+++ Imi.Project.Mobile/Imi.Project.Mobile/Domain/Models/TheMealDbRecipe.cs
@@ -0,0 +1,10 @@
+﻿namespace Imi.Project.Mobile.Domain.Models
+{
+	public class TheMealDbRecipe
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Category { get; set; }
+		public string Instructions { get; set; }
+	}
+}
