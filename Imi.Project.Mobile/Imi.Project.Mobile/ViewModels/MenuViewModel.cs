f45b42b4839acf93437128201e65728304c2c9cd
woutercallewaert <wouter.callewaert@student.howest.be>
2023-10-09 00:33:33 +0200

    Add navigation menu

---
+++ Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/MenuViewModel.cs
@@ -0,0 +1,49 @@
+﻿using FreshMvvm;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+    public class MenuViewModel : FreshBasePageModel
+    {
+        public MenuViewModel()
+        {
+
+        }
+
+        public ICommand OpenRecipeCreationPageCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<RecipeCreationViewModel>(true);
+            }
+            );
+
+        public ICommand OpenMyRecipesPageCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<MyRecipesViewModel>(true);
+            }
+            );
+
+        public ICommand OpenRecipesPageCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<RecipesViewModel>(true);
+            }
+            );
+
+		public ICommand OpenTheMealDbPageCommand => new Command(
+	        async () =>
+	        {
+                await CoreMethods.PushPageModel<TheMealDbViewModel>(true);
+            }
+	        );
+
+		public ICommand CloseAppPageCommand => new Command(
+            () =>
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+			}
+            );               
+    }
+}
