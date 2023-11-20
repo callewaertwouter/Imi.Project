ec30162cbf90c5eff62dd8989e6738b69a94a606
woutercallewaert <wouter.callewaert@student.howest.be>
2023-11-20 11:24:35 +0100

    Add viewmodels

---
+++ Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/LoginViewModel.cs
@@ -0,0 +1,77 @@
+﻿using FreshMvvm;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Imi.Project.Mobile.Domain.Models;
+using Xamarin.Essentials;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+	public class LoginViewModel : FreshBasePageModel
+	{
+		public string Email { get; set; }
+		public string Password { get; set; }
+
+		public LoginViewModel()
+		{
+
+		}
+
+		public ICommand LoginCommand => new Command(
+			async () =>
+			{
+				if (!string.IsNullOrWhiteSpace(Email) && !string.IsNullOrWhiteSpace(Password))
+				{
+					try
+					{
+						// Attempt to dodge SSL certificate validation error
+						System.Net.ServicePointManager.ServerCertificateValidationCallback +=
+							(sender, certificate, chain, sslPolicyErrors) => true;
+
+						var loginData = new
+						{
+							Email = Email,
+							Password = Password
+						};
+
+						var jsonData = JsonConvert.SerializeObject(loginData);
+						var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+						using (var httpClient = new HttpClient())
+						{
+							var apiUrl = "https://localhost:5001/api/Users/api/auth/login";
+
+							var response = await httpClient.PostAsync(apiUrl, content);
+							if (response.IsSuccessStatusCode)
+							{
+								var responseContent = await response.Content.ReadAsStringAsync();
+								var tokenResponse = JsonConvert.DeserializeObject<TokenResponseModel>(responseContent);
+
+								// Store token
+								await SecureStorage.SetAsync("JWTBearer", tokenResponse.Token);
+
+								await CoreMethods.PushPageModel<MenuViewModel>(true);
+							}
+							else
+							{
+								// Authentication failure
+								await CoreMethods.DisplayAlert("Login Failed", "Incorrecte gegevens.", "OK");
+							}
+						}
+					}
+					catch (HttpRequestException ex)
+					{
+						await CoreMethods.DisplayAlert("Error", "An error occurred: " + ex.Message + ex.InnerException, "OK");
+					}
+				}
+				else
+				{
+					await CoreMethods.DisplayAlert("Error", "Vul alle velden in.", "OK");
+				}
+			});
+	}
+}
+
--- Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/RecipesViewModel.cs
+++ Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/RecipesViewModel.cs
@@ -1,11 +1,51 @@
 ﻿using FreshMvvm;
-using System;
+using Imi.Project.Mobile.Domain.Models;
+using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Net.Http;
 
 namespace Imi.Project.Mobile.ViewModels
 {
-	internal class RecipesViewModel : FreshBasePageModel
+	public class RecipesViewModel : FreshBasePageModel
 	{
+		public ObservableCollection<string> Recipes { get; set; }
+
+		public RecipesViewModel()
+		{
+			Recipes = new ObservableCollection<string>();
+			LoadRecipes();
+		}
+
+		private async void LoadRecipes()
+		{
+			try
+			{
+				using (var httpClient = new HttpClient())
+				{
+					var apiUrl = "https://localhost:5001/api/Recipes";
+					var response = await httpClient.GetAsync(apiUrl);
+
+					if (response.IsSuccessStatusCode)
+					{
+						var content = await response.Content.ReadAsStringAsync();
+						var recipes = JsonConvert.DeserializeObject<List<Recipe>>(content);
+
+						foreach (var recipe in recipes)
+						{
+							Recipes.Add(recipe.Title);
+						}
+					}
+					else
+					{
+						await CoreMethods.DisplayAlert("Probleem", "Iets misgelopen.", "OK");
+					}
+				}
+			}
+			catch (HttpRequestException ex)
+			{
+				await CoreMethods.DisplayAlert("Error", "An error occurred: " + ex.Message + ex.InnerException, "OK");
+			}
+		}
 	}
 }
